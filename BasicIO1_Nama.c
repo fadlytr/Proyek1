868a4c1844ed6e31c15cac42fd4900a1dba9b3e3
fadlytr <36650539+fadlytr@users.noreply.github.com>
2018-02-20 08:56:34 +0700

    Add files via upload

---
+++ BasicIO1_Nama.c
@@ -0,0 +1,22 @@
+/*=======================================
+Program 		: BasicIO1_Nama.c
+Deskripsi 		: Hasil 2 kalinya x
+Nama/NIM		: Fadly Triansyah Rahman/171524008
+versi/Tanggal	: 1/21 oktober 2017
+Compiler		: Devc++ 5.11
+=======================================*/
+#include <stdio.h>
+int main() 
+{
+	/* Deklarasi*/
+	int x;
+	
+	/* Algoritma*/
+	printf ("Masukan Angka dari 0 hingga 100 : ");
+	scanf ("%d", &x);
+	if ((x <= 100) && (x >= 0))
+		printf ("%d\n",x*2);
+	else 
+		printf ("Angka yang dimasukkan Harus 0 hingga 100");
+	return 0;
+}
